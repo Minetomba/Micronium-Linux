ab6d166221e6eadddf621ab64949416f8177432b
Minetomba <147551445+Minetomba@users.noreply.github.com>
2026-09-20 11:47:30 +0300

    38

--- src/main.c
+++ src/main.c
@@ -322,73 +322,46 @@ int main(int argc, char* argv[]) {
 			if (strcmp(argv[1], "msli") == 0) { // Micronium stack language interpreter (msli)
 				intptr_t stack[4096];
 				intptr_t stack_pointer = 0;
-				char c;
-				intptr_t n;
 				size_t pc = 0;
 				int in_construct = 0;
 				int last_construct = 0;
-				intptr_t in = syscall(SYS_open, argv[2], O_RDONLY);
-				if (in < 0) {
-					return 1;
-				}
-				struct stat st;
-				if (syscall(SYS_fstat, in, &st) < 0) {
-					return 1;
-				}
-				size_t prog_size = st.st_size;
-				char *prog = malloc(prog_size);
-				if (!prog) {
-					return 1;
-				}
-				size_t got = 0;
-				while (got < prog_size) {
-					ssize_t r = syscall(SYS_read, in, prog + got, prog_size - got);
-					if (r <= 0) {
-						break;
-					}
-					got += r;
-				}
-				syscall(SYS_close, in);
-				while (pc < prog_size) {
-					char c = prog[pc];
+				while (argv[2][pc] != '\0') {
+					char c = argv[2][pc];
 					if (c != '0' && c != '1' && c != '2' && c != '3' && c != '4' && c != '5' && c != '6' && c != '7' && c != '8' && c != '9' && c != '-' && in_construct == 1) { /* Pushing the constructed number */
 						in_construct = 0;
 						stack_pointer += 1;
 						stack[stack_pointer] = last_construct;
 						last_construct = 0;
-						pc += 1;
-						continue;
-					} else if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' || c == '-' && in_construct == 0) { /* Constructing the number */
+					} else if (c >= '0' && c <= '9') { /* Constructing the number */
 						in_construct = 1;
-						last_construct *= 10;
-						last_construct += ((int)c) - 48;
-						pc += 1;
-						continue;
+						last_construct = last_construct + last_construct + last_construct + last_construct + last_construct + last_construct + last_construct + last_construct + last_construct + last_construct;
+						last_construct += ((int)c) + -48;
+					} else if (c == '-') {
+						last_construct = (~last_construct) + 1;
 					} else if (c == '@') { /* Get */
 						stack[stack_pointer] = *(intptr_t*)stack[stack_pointer];
 					} else if (c == '!') { /* Store */
-						*(intptr_t*)stack[stack_pointer] = stack[stack_pointer - 1];
-						stack_pointer -= 2;
+						*(intptr_t*)stack[stack_pointer] = stack[stack_pointer + -1];
+						stack_pointer += -2;
 					} else if (c == '+') { /* Add */
-						stack[stack_pointer - 1] = stack[stack_pointer - 1] + stack[stack_pointer];
-						stack_pointer -= 1;
+						stack[stack_pointer + -1] = stack[stack_pointer + -1] + stack[stack_pointer];
+						stack_pointer += -1;
 					} else if (c == '?') { /* Branch */
 						if (stack[stack_pointer - 1] < stack[stack_pointer]) {
-							pc = stack[stack_pointer - 2];
-							stack_pointer -= 3;
+							pc = stack[stack_pointer + -2];
+							stack_pointer += -3;
 							continue;
 						}
-						stack_pointer -= 3;
+						stack_pointer += -3;
 					} else if (c == '$') { /* Stack base address */
 						stack_pointer += 1;
 						stack[stack_pointer] = (intptr_t)&stack[0];
 					} else if (c == '.') { /* Output */
 						printf("%c", (char)stack[stack_pointer]);
-						stack_pointer -= 1;
+						stack_pointer += -1;
 					}
 					pc += 1;
 				}
-				free(prog);
 			}
 		}
 		if (argc == 4) {
